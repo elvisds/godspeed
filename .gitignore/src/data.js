e4be4592e5a79419e0bbe2a1371ad2608635734e
Elvis D'Souza <me@elvis.co.in>
2017-07-02 23:11:09 +0530

    Bug fix

--- .gitignore/src/data.js
+++
@@ -1,9 +0,0 @@
-import _ from 'underscore';
-
-var DRIVER_NAMES = ['Abhiram', 'Arjun', 'Arnav', 'Aryan', 'Atharv', 'Ayush', 'Krishna', 'Pranav', 'Ryan', 'Sai', 'Samarth', 'Siddharth'];
-
-export default class Data {
-  getDrivers(num_drivers) {
-    return _.sample(DRIVER_NAMES, num_drivers);
-  }
-}
